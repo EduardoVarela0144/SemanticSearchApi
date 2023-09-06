b43faf25b63671571958ed37a591b0544e64587a
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-09-06 00:22:41 -0600

    "Web flask design"

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
+import os
+from flask import Flask, request, render_template
+
 import stanza
 
-nlp = stanza.Pipeline(lang='en', processors='tokenize,lemma,pos,ner,depparse')
+app = Flask(__name__)
 
-file_path = 'text_files/PMC1249490.txt'
+# Descarga e instala los modelos de idioma en inglés
+stanza.download('en')
 
-with open(file_path, 'r', encoding='utf-8') as file:
-    text = file.read()
+# Inicializa el modelo de idioma en inglés
+nlp = stanza.Pipeline('en')
 
-doc = nlp(text)
+# Ruta donde se guardarán los archivos subidos
+UPLOAD_FOLDER = 'uploaded_files'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
-ner_results = []
-dependency_results = []
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        archivo = request.files['file']
+        if not archivo:
+            return "Por favor, seleccione un archivo para subir."
 
-for sentence in doc.sentences:
-    for token in sentence.tokens:
-        word = token.text
-        ner = token.ner
-        dependency_results.append((word))
-        ner_results.append((word, ner))
+        # Verifica la extensión del archivo
+        if archivo.filename.endswith('.txt'):
+            # Guarda el archivo en el directorio de carga
+            filename = os.path.join(UPLOAD_FOLDER, archivo.filename)
+            archivo.save(filename)
 
-print("Resultados de NER:")
-for word, ner in ner_results:
-    print(f"Palabra: {word}, NER: {ner}")
+            # Procesa el texto del archivo
+            with open(filename, "r", encoding="utf-8") as file:
+                texto = file.read()
 
-print("\nResultados de Análisis de Dependencias:")
-for word, head, dependency in dependency_results:
-    print(f"Palabra: {word}, Head: {head}, Dependencia: {dependency}")
+            # Realiza el análisis de POS y agrupa las palabras
+            doc = nlp(texto)
+
+            palabras_por_etiqueta = {}
+            etiquetas_a_excluir = {"AUX", "ADP", "PRON", "NUM", "X", "PART", "SYM", "SCONJ", "CCONJ", "DET", "PUNCT"}
+
+            for sentence in doc.sentences:
+                for word in sentence.words:
+                    etiqueta_pos = word.pos
+                    if etiqueta_pos not in etiquetas_a_excluir:
+                        if etiqueta_pos not in palabras_por_etiqueta:
+                            palabras_por_etiqueta[etiqueta_pos] = []
+                        palabras_por_etiqueta[etiqueta_pos].append(word.text)
+
+            # Elimina el archivo cargado
+            os.remove(filename)
+
+            # Retorna las palabras agrupadas por etiqueta POS
+            return render_template('result.html', palabras_por_etiqueta=palabras_por_etiqueta)
+        else:
+            return "El archivo debe tener la extensión .txt."
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
