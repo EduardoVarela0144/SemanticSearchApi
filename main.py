36ee6683d7a27576d5e85d64bd7ddde0c9135265
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-11-01 16:25:31 -0600

    Articles routes fix

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from routes.article_routes import semantic_api
+from routes.article_routes import articles_routes
 import os
 import stanza
 import spacy
 
 app = Flask(__name__)
+app.register_blueprint(articles_routes)
+
 stanza.download('en')
 nlp = spacy.load("en_core_web_sm")
 
@@ -12,7 +14,7 @@ UPLOAD_FOLDER = 'static'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-    app.register_blueprint(semantic_api)
 
--- routes/article_routes.py
+++ routes/article_routes.py
@@ -1,26 +1,26 @@
 from flask import request, Blueprint
 from controllers.article_controller import ArticleController
 
-semantic_api = Blueprint('articles', __name__)
+articles_routes = Blueprint('articles', __name__)
 
 article_controller = ArticleController()
 
-@semantic_api.route('/articles', methods=['POST'])
+@articles_routes.route('/articles', methods=['POST'])
 def create_article():
     return article_controller.create_article(request)
 
-@semantic_api.route('/articles/<article_id>', methods=['GET'])
+@articles_routes.route('/articles/<article_id>', methods=['GET'])
 def get_article(article_id):
     return article_controller.get_article(article_id)
 
-@semantic_api.route('/articles/<article_id>', methods=['PUT'])
+@articles_routes.route('/articles/<article_id>', methods=['PUT'])
 def update_article(article_id):
     return article_controller.update_article(request, article_id)
 
-@semantic_api.route('/articles/<article_id>', methods=['DELETE'])
+@articles_routes.route('/articles/<article_id>', methods=['DELETE'])
 def delete_article(article_id):
     return article_controller.delete_article(article_id)
 
-@semantic_api.route('/articles', methods=['GET'])
+@articles_routes.route('/articles', methods=['GET'])
 def search_articles():
     return article_controller.search_articles(request)
