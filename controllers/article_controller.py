734edcab429bc169b1171e9222042194a7365b4c
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-11-14 17:33:03 -0600

    Fix luisfe cagadero

--- controllers/article_controller.py
+++ controllers/article_controller.py
@@ -5,6 +5,10 @@ from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import NotFoundError
 from stanza.server import CoreNLPClient
 
+import os
+from models.article import Article
+from werkzeug.utils import secure_filename
+
 class ArticleController:
     def __init__(self):
         # Configuración de spaCy para inglés
@@ -13,6 +17,59 @@ class ArticleController:
         # Configuración de Elasticsearch
         self.es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
 
+    def create_article(self, request):
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'})
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'})
+
+        folder = request.form.get('folder', 'articles')  
+        folder_path = os.path.join('static', folder)
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        if file:
+            filename = os.path.join(folder_path, secure_filename(file.filename))
+
+            file.save(filename)
+
+            data = request.form.to_dict()
+            article = Article(**data, path=folder)
+            article.save()
+
+            return jsonify({'message': 'Article created successfully', 'path': f'static/{folder}'})
+
+
+    def get_article(self, article_id):
+        article = Article.find_by_id(article_id)
+        return jsonify(article.json())
+
+    def update_article(self, request, article_id):
+        data = request.get_json()
+        article = Article.find_by_id(article_id)
+        if article:
+            article.update(data)
+            return jsonify({'message': 'Updated article'})
+        else:
+            return jsonify({'message': 'Article not found'}, 404)
+
+    def delete_article(self, article_id):
+        article = Article.find_by_id(article_id)
+        if article:
+            article.delete()
+            return jsonify({'message': 'Article removed'})
+        else:
+            return jsonify({'message': 'Article not found'}, 404)
+
+    def search_articles(self, request):
+        query = request.args.get('query')
+        articles = Article.search(query)
+        return jsonify([article.json() for article in articles])
+    
     def analyze_documents(self, path):
         try:
             result_collection = []
@@ -68,13 +125,10 @@ class ArticleController:
 
         with CoreNLPClient(annotators=["openie"], be_quiet=False, ) as client:
             for text in sentences:
-                ann = client.annotate(sentences)
+                ann = client.annotate(text)
                 for sentence in ann.sentence:
                     triplet_sentence = []
                     for triple in sentence.openieTriple:
-                        print("Subject:", triple.subject)
-                        print("Relation:", triple.relation)
-                        print("Object:", triple.object)
 
                         triplet = {
                         'subject': triple.subject,
@@ -85,7 +139,7 @@ class ArticleController:
 
                     if triplet_sentence:
                         sentences_and_triplets.append({
-                            'sentence_number': num,
+                            #'sentence_number': num,
                             'sentence_text': sentence.text if sentence.text else "Not Found",
                             'triplets': triplet_sentence,
                         })
