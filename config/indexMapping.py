4cc9770069b81f9f1b972e1b87b61bf97568af5f
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-12-02 13:44:16 -0600

    indexMappging

---
+++ config/indexMapping.py
@@ -0,0 +1,53 @@
+indexMapping = {
+    "properties": {
+        "title": {
+            "type": "text"
+        },
+        "authors": {
+            "type": "text"
+        },
+        "journal": {
+            "type": "text"
+        },
+        "issn": {
+            "type": "text"
+        },
+        "doi": {
+            "type": "text"
+        },
+        "pmc_id": {
+            "type": "text"
+        },
+        "keys": {
+            "type": "text"
+        },
+        "abstract": {
+            "type": "text"
+        },
+        "objectives": {
+            "type": "text"
+        },
+        "methods": {
+            "type": "text"
+        },
+        "content": {
+            "type": "text"
+        },
+        "results": {
+            "type": "text"
+        },
+        "conclusion": {
+            "type": "text"
+        },
+        "path": {
+            "type": "text"
+        },
+        "DescriptionVector": {
+            "type": "dense_vector",
+            "dims": 768,
+            "index": True,
+            "similarity": "l2_norm"
+        }
+
+    }
+}
