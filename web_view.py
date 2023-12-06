b9feaa2af53aee7a24df3fc61c451ddf60451ae5
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-12-06 10:36:53 -0600

    web view example

---
+++ web_view.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from elasticsearch import Elasticsearch
+from sentence_transformers import SentenceTransformer
+
+indexName = "articles"
+
+try:
+    es = Elasticsearch(
+        "https://localhost:9200",
+        basic_auth=("elastic", "SZoY=mikTz4MCctIcWhX"),
+        ca_certs="/Users/varela/http_ca.crt"
+    )
+
+except ConnectionError as e:
+    print("Connection Error:", e)
+    
+if es.ping():
+    print("Succesfully connected to ElasticSearch!!")
+else:
+    print("Oops!! Can not connect to Elasticsearch!")
+
+
+
+
+def search(input_keyword):
+    model = SentenceTransformer('all-mpnet-base-v2')
+    vector_of_input_keyword = model.encode(input_keyword)
+
+    query = {
+        "field": "vector",
+        "query_vector": vector_of_input_keyword,
+        "k": 10,
+        "num_candidates": 500
+    }
+    res = es.knn_search(index="articles"
+                        , knn=query 
+                        , source=["title","content"]
+                        )
+    results = res["hits"]["hits"]
+
+    return results
+
+def main():
+    st.title("Razonamiento neuronal de dominio abierto para la reconstrucción e inferencia de conocimiento: una aplicación en enfermedades respiratorias")
+
+    # Input: User enters search query
+    search_query = st.text_input("Enter your search query")
+
+    # Button: User triggers the search
+    if st.button("Search"):
+        if search_query:
+            # Perform the search and get results
+            results = search(search_query)
+
+            # Display search results
+            st.subheader("Search Results")
+            for result in results:
+                with st.container():
+                    if '_source' in result:
+                        try:
+                            st.header(f"{result['_source']['title']}")
+                        except Exception as e:
+                            print(e)
+                        
+                        try:
+                            st.write(f"Content: {result['_source']['content']}")
+                        except Exception as e:
+                            print(e)
+                        st.divider()
+
+                    
+if __name__ == "__main__":
+    main()
