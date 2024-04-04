bb80700e170fd3289774f56fb9ff2ba07394ef54
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2024-04-04 13:07:27 -0600

    fix triplets size

--- controllers/statistics_controller.py
+++ controllers/statistics_controller.py
@@ -12,15 +12,13 @@ class StatisticsController:
     def get_index_counts(self):
         try:
             user_count = self.get_index_count('users')
-            triplets_count = self.get_index_count('triplets')
+            triplets_count = self.get_triplets_count('triplets')
             articles_count = self.get_index_count('articles')
-            # triplets_vector_count = self.get_index_count('triplets_vector')
-
+            
             return jsonify({
                 'users': user_count,
                 'triplets': triplets_count,
                 'articles': articles_count,
-                # 'triplets_vector': triplets_vector_count
             })
 
         except Exception as e:
@@ -34,6 +32,28 @@ class StatisticsController:
                 }
             })
             return response.get('count', 0)
+
+        except NotFoundError:
+            return 0
+    
+    def get_triplets_count(self, index_name):
+        try:
+            count = 0
+            scroll = self.es.search(index=index_name, scroll='1m', size=10000, body={
+                "query": {
+                    "exists": {"field": "triplets"}
+                },
+                "_source": ["triplets"]
+            })
+
+            while len(scroll['hits']['hits']) > 0:
+                for hit in scroll['hits']['hits']:
+                    triplets = hit['_source'].get('triplets', [])
+                    count += len(triplets)
+                
+                scroll = self.es.scroll(scroll_id=scroll['_scroll_id'], scroll='1m')
+            
+            return count
 
         except NotFoundError:
             return 0
